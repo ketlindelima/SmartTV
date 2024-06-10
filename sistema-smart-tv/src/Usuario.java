de06c60d429c55add9fc6e4f2a44ee2bdd6c6672
Ketlin de Lima <ketlindelima@outlook.com>
2024-06-09 21:24:12 -0300

    Initial project

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,10 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.ligar();
+        System.out.println(smartTv.ligada);
+
+    }
+}
